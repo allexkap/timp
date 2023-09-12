a4c2687549d1cd31071c3edab3a8673456ea25c9
allexkap <aekpstn@gmail.com>
2023-09-13 00:44:41 +0300

    fix poc

--- lab2/poc.c
+++ lab2/poc.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int check() {
+#define SIZE 2*PATH_MAX+42
+int check(const char* name) {
     static volatile char* sign = "~~llxkp";
-    system("sed 's/~>\\?llxkp/llxkp\\x0/' poc -i~\nrm poc~\n");
-    return sign[0] == '~';
+    static char* raw = "sed 's/~>\\?llxkp/llxkp\\x0/' %s -i~\nrm %s~\n";
+    char cmd[SIZE];
+    snprintf(cmd, SIZE, raw, name, name);
+    return system(cmd) || sign[0] != '~';
 }
 
-int main() {
-    printf(check()?":)\n":":(\n");
+int main(int argc, char** argv) {
+    printf(check(argv[0])?":(\n":":)\n");
     return 0;
 }

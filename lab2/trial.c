ba3b5b27f7b2e627ae0221088cc2e11631b84096
allexkap <aekpstn@gmail.com>
2023-10-03 18:51:15 +0300

    add work sim

--- lab2/trial.c
+++ lab2/trial.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#define LEFT 2
+#include <string.h>
+#define LEFT 4
 
 
 int main() {
 
     int left = LEFT;
 
-    FILE* file = fopen("/tmp/trial", "r+");
+    FILE* file = fopen("/tmp/trial.left", "r+");
     if (file) {
         fscanf(file, "%d", &left);
         fseek(file, 0, SEEK_SET);
     }
     else {
-        file = fopen("/tmp/trial", "w");
+        file = fopen("/tmp/trial.left", "w");
     }
     fprintf(file, "%d", --left);
     fclose(file);
@@ -23,5 +24,31 @@ int main() {
     }
 
     printf("%d runs left\n", left);
+
+
+    char *name = NULL;
+    printf("> ");
+    getline(&name, &(size_t){0}, stdin);
+
+    file = fopen("/tmp/trial.data", "r+");
+    if (!file) {
+        file = fopen("/tmp/trial.data", "w");
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+
+    while (getline(&line, &len, file) > 0) {
+        if (strcmp(line, name)) continue;
+        printf("request found in cache\n");
+        goto exit;
+    }
+
+    fseek(file, 0, SEEK_END);
+    fprintf(file, "%s", name);
+
+    exit:
+    fclose(file);
+
     return 0;
 }

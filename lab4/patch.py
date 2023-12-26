7e84af4a594f1f500d381f6784eb9d5af3aa45e8
allexkap <aekpstn@gmail.com>
2023-12-26 23:52:07 +0300

    add colors

--- lab4/patch.py
+++ lab4/patch.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 
 code = (
@@ -6,22 +6,29 @@ code = (
     b'\x08\x84\xC0\x00\x00',
 )
 
-for entry in os.listdir('.'):
+
+c = {
+    '': '\033[0m',
+    'r': '\033[31;1m',
+    'g': '\033[32;1m',
+}
+
+for entry in Path('.').glob('*'):
     try:
-        if not os.path.isdir(entry):
+        if not entry.is_dir():
             continue
-        with open(f'{entry}/LabReverse.exe', 'rb+') as file:
+        with open(entry / 'LabReverse.exe', 'rb+') as file:
             data = file.read()
-            c = data.count(code[0])
-            if c < 0:
-                print(f'{entry}: not found')
-            elif c > 1:
-                print(f'{entry}: multiple matches')
+            n = data.count(code[0])
+            if n < 1:
+                print(f'{entry}: {c["r"]}not found{c[""]}')
+            elif n > 1:
+                print(f'{entry}: {c["r"]}multiple matches{c[""]}')
             else:
                 data = data.replace(code[0], code[1])
                 file.seek(0)
                 file.write(data)
                 file.truncate()
-                print(f'{entry}: successfully')
+                print(f'{entry}: {c["g"]}successfully{c[""]}')
     except Exception as e:
         print(f'{entry}: {e}')
